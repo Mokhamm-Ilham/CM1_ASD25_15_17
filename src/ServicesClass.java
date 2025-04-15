4c8b4ed4af7940b6f91094a6af954ff2b10c2cdc
RizkyFerdi <mazrizky46@gmail.com>
2025-04-15 09:17:10 +0700

    add File TransaksiPembelian & Services Class

---
+++ src/ServicesClass.java
@@ -0,0 +1,19 @@
+public class ServicesClass {
+ TransaksiPembelian[] Trs;
+ int index;
+
+ void tambahDataTransaksi(TransaksiPembelian trp) {
+
+ }
+
+ int searching() {
+  return 0;
+ }
+
+ void Sorting() {
+ }
+
+ void UpdateData() {
+ }
+
+}
---
+++ src/TransaksiPembelian.java
@@ -0,0 +1,14 @@
+public class TransaksiPembelian {
+
+ String kodeTransaki;
+ String namaPembeli;
+ String tanggalPembelian;
+ int qty;
+
+ TransaksiPembelian(String kodeTransaksi, String namaPembeli, String tanggalPembelian, int qty) {
+ }
+
+ void tampilDataTransaksi() {
+ }
+
+}
